2cfb7058bb6c9e88a990887720489e8d9d0dce65
BioNinja45 <dabob45@gmail.com>
2016-12-11 16:51:41 -0600

    fixed sam's mess up

--- client/platformer.js
+++ client/platformer.js
@@ -15,24 +15,18 @@ var images = [
   new Image(),
   new Image(),
   new Image(),
+  new Image(),
   new Image()
 ];
 
 images[0].src = 'level.png'; // Background
-images[1].src = 'stars.jpg'; // Foreground
+images[1].src = 'stars.png'; // Foreground
 images[2].src = 'fumiko2.png';  // Player
+images[3].src = 'DownArrow.png'; // Down arrow above hiding objects
+images[4].src = 'BrownBox.png'; // Brown box
+images[5].src = 'GrayBox.png'; // Gray box
+images[6].src = 'Enemy_Ship.png'; // enemy
 
-var hidingObjImages = [
-new Image(),
-new Image(),
-new Image(),
-new Image()
-];
-
-hidingObjImages[0].src = 'DownArrow.png'; // Down arrow above hiding objects
-hidingObjImages[1].src = 'BrownBox.png'; // Brown box
-hidingObjImages[2].src = 'GrayBox.png'; // Gray box
-hidingObjImages[3].src = 'BrownBox.png'; // Brown box
 
 
 // Start the game after all files have loaded
@@ -148,7 +142,7 @@ function renderBackground(ctx, current) {
   ctx.save();
   //TODO: magic numbers. 19200 = width of level image in pixels. 11229 = canvas height/image height * image width
   ctx.drawImage(images[0],
-                (images[0].width/(canvas.height/(images[0].height)*images[0].width))*(current.levelPos.x - current.screenPos.x), 0, images[0].width, images[0].height,
+                (19200/11229)*(current.levelPos.x - current.screenPos.x), 0, images[0].width, images[0].height,
                 0, 0, 11229, HEIGHT);
   // ctx.drawImage(images[0], 0, 0, images[0].width, HEIGHT);
   ctx.restore();
@@ -165,17 +159,41 @@ function renderPlayers(players, ctx) {
   ctx.fillText('screen: (' + Math.floor(players.current.screenPos.x)+ ',' + Math.floor(players.current.screenPos.y) + ')', players.current.screenPos.x, players.current.screenPos.y - 10);
   ctx.fillText('level: (' + Math.floor(players.other.levelPos.x) + ',' + Math.floor(players.other.levelPos.y) + ')', players.other.levelPos.x - players.current.levelPos.x + players.current.screenPos.x, players.other.screenPos.y - 30);
   ctx.fillText('screen: (' + Math.floor(players.other.screenPos.x)+ ',' + Math.floor(players.other.screenPos.y) + ')', players.other.levelPos.x - players.current.levelPos.x + players.current.screenPos.x, players.other.screenPos.y - 10);  
+  if(players.current.id=='player') {
+	  // Draw current player's sprite
+	  ctx.drawImage( images[2],players.current.sx ,
+	   players.current.sy, players.current.swidth, players.current.sheight,
+	   players.current.screenPos.x, players.current.screenPos.y, players.current.width, players.current.height);
   
-  // Draw current player's sprite
-  ctx.drawImage( images[2],players.current.sx ,
-   players.current.sy, players.current.swidth, players.current.sheight,
-   players.current.screenPos.x, players.current.screenPos.y, players.current.width, players.current.height);
-
+  // Draw other player's sprite
+  ctx.drawImage( images[6],players.other.sx ,
+   players.other.sy, players.other.swidth, players.other.sheight,
+   players.other.levelPos.x - players.current.levelPos.x + players.current.screenPos.x, players.other.screenPos.y, players.other.width, players.other.height);   
+  }
+else{
+	console.log(players.current);
+	ctx.drawImage( images[6],players.current.sx ,
+	   players.current.sy, players.current.swidth, players.current.sheight,
+	   players.current.screenPos.x, players.current.screenPos.y, players.current.width, players.current.height);
+  
   // Draw other player's sprite
   ctx.drawImage( images[2],players.other.sx ,
    players.other.sy, players.other.swidth, players.other.sheight,
-   players.other.levelPos.x - players.current.levelPos.x + players.current.screenPos.x, players.other.screenPos.y, players.other.width, players.other.height);   
-
+   players.other.levelPos.x - players.current.levelPos.x + players.current.screenPos.x, players.other.screenPos.y, players.other.width, players.other.height);
+	
+  ctx.save();
+  ctx.translate(reticule.x, reticule.y);
+  ctx.beginPath();
+  ctx.arc(0, 0, 10, 0, 2*Math.PI);
+  ctx.moveTo(0, 15);
+  ctx.lineTo(0, -15);
+  ctx.moveTo(15, 0);
+  ctx.lineTo(-15, 0);
+  ctx.strokeStyle = '#00ff00';
+  ctx.stroke();
+  ctx.restore();
+   
+   }
   ctx.restore();
 }
 
@@ -195,8 +213,8 @@ function renderHidingObjects (players, hidingObjects, ctx, renderDelayedObjs)
 	  {
 		  if(hidingObjects.objects[i].delayRender == false)
 		  {
-			ctx.drawImage(hidingObjImages[hidingObjects.objects[i].type],  hidingObjects.objects[i].position.x + (players.current.screenPos.x - players.current.levelPos.x), 
-			hidingObjects.objects[i].position.y, hidingObjImages[hidingObjects.objects[i].type].width * 2, hidingObjImages[hidingObjects.objects[i].type].height * 2); 
+			ctx.drawImage(images[4 + hidingObjects.objects[i].type],  hidingObjects.objects[i].position.x + (players.current.screenPos.x - players.current.levelPos.x), 
+			hidingObjects.objects[i].position.y, images[4 + hidingObjects.objects[i].type].width * 2, images[4 + hidingObjects.objects[i].type].height * 2); 
 		  }
 	  }
 	  // Now render those being hid behind
@@ -204,79 +222,20 @@ function renderHidingObjects (players, hidingObjects, ctx, renderDelayedObjs)
 	  {
 		  if(hidingObjects.objects[i].delayRender == true)
 		  {
-			ctx.drawImage(hidingObjImages[hidingObjects.objects[i].type],  hidingObjects.objects[i].position.x + (players.current.screenPos.x - players.current.levelPos.x), 
-			hidingObjects.objects[i].position.y, hidingObjImages[hidingObjects.objects[i].type ].width * 2, hidingObjImages[hidingObjects.objects[i].type].height * 2); 
+			ctx.drawImage(images[4 + hidingObjects.objects[i].type],  hidingObjects.objects[i].position.x + (players.current.screenPos.x - players.current.levelPos.x), 
+			hidingObjects.objects[i].position.y, images[4 + hidingObjects.objects[i].type ].width * 2, images[4 + hidingObjects.objects[i].type].height * 2); 
 		  }
 	  }
 	  
 	 // Display animated down arrow if on top of a object that can be hid behind
 	 if(hidingObjects.objects[i].displayArrow == true)
 	{
-		ctx.drawImage(hidingObjImages[0],hidingObjects.arrowFrame * 130, 0, 128, 175,  hidingObjects.objects[i].position.x + (players.current.screenPos.x - players.current.levelPos.x) + 16, hidingObjects.objects[i].position.y - 95, 30, 45);
+		ctx.drawImage(images[3],hidingObjects.arrowFrame * 130, 0, 128, 175,  hidingObjects.objects[i].position.x + (players.current.screenPos.x - players.current.levelPos.x) + 16, hidingObjects.objects[i].position.y - 95, 30, 45);
 	}
   }
   ctx.restore();
   
 }
-
-// function renderPlayerView(players, ctx) {
-//   // Draw the canvas backgrounds
-//   if(players.player.direction == 'none' || players.enemy.direction == 'none') {
-//     renderBackground(ctx, players.player);
-//   }
-//   //console.log(players.player);
-//   //ctx.fillRect(players.player.x, players.player.y, 5, 5);
-//   ctx.save();
-//   // ctx.translate(-camera_position.x, players.player.y);
-//   ctx.fillStyle = 'white';
-//   ctx.font="20px Verdana";
-//   ctx.fillText('level: (' + Math.floor(players.player.levelPos.x) + ',' + Math.floor(players.player.levelPos.y) + ')', players.player.screenPos.x, players.player.screenPos.y - 30);
-//   ctx.fillText('screen: (' + Math.floor(players.player.screenPos.x)+ ',' + Math.floor(players.player.screenPos.y) + ')', players.player.screenPos.x, players.player.screenPos.y - 10);
-//   ctx.fillText('level: (' + Math.floor(players.enemy.levelPos.x) + ',' + Math.floor(players.enemy.levelPos.y) + ')', players.enemy.levelPos.x - players.player.levelPos.x + players.player.screenPos.x, players.enemy.screenPos.y - 30);
-//   ctx.fillText('screen: (' + Math.floor(players.enemy.screenPos.x)+ ',' + Math.floor(players.enemy.screenPos.y) + ')', players.enemy.levelPos.x - players.player.levelPos.x + players.player.screenPos.x, players.enemy.screenPos.y - 10);  
-
-//   // Draw player sprite
-//   ctx.drawImage( images[2],players.player.sx ,
-//    players.player.sy, players.player.swidth, players.player.sheight,
-//    players.player.screenPos.x, players.player.screenPos.y, players.player.width, players.player.height);
-
-//   // Draw enemy sprite
-//   ctx.drawImage( images[2],players.enemy.sx ,
-//    players.enemy.sy, players.enemy.swidth, players.enemy.sheight,
-//    players.enemy.levelPos.x - players.player.levelPos.x + players.player.screenPos.x, players.enemy.screenPos.y, players.enemy.width, players.enemy.height);
-
-
-//   ctx.restore();
-// }
-
-// function renderEnemyView(players, ctx) {
-//   // Draw the canvas backgrounds
-//   if(players.player.direction == 'none' || players.enemy.direction == 'none') {
-//     renderBackground(ctx, players.enemy);
-//   }
-//   //console.log(players.player);
-//   //ctx.fillRect(players.player.x, players.player.y, 5, 5);
-//   ctx.save();
-//   // ctx.translate(-camera_position.x, players.player.y);
-//   ctx.fillStyle = 'white';
-//   ctx.font="20px Verdana";
-//   ctx.fillText('level: (' + Math.floor(players.enemy.levelPos.x) + ',' + Math.floor(players.enemy.levelPos.y) + ')', players.enemy.screenPos.x, players.enemy.screenPos.y - 30);
-//   ctx.fillText('screen: (' + Math.floor(players.enemy.screenPos.x)+ ',' + Math.floor(players.enemy.screenPos.y) + ')', players.enemy.screenPos.x, players.enemy.screenPos.y - 10);
-//   ctx.fillText('level: (' + Math.floor(players.player.levelPos.x) + ',' + Math.floor(players.player.levelPos.y) + ')', players.player.levelPos.x - players.enemy.levelPos.x + players.enemy.screenPos.x, players.player.screenPos.y - 30);
-//   ctx.fillText('screen: (' + Math.floor(players.player.screenPos.x)+ ',' + Math.floor(players.player.screenPos.y) + ')', players.player.levelPos.x - players.enemy.levelPos.x + players.enemy.screenPos.x, players.player.screenPos.y - 10);  
-
-//   // Draw enemy sprite
-//   ctx.drawImage( images[2],players.enemy.sx ,
-//    players.enemy.sy, players.enemy.swidth, players.enemy.sheight,
-//    players.enemy.screenPos.x, players.enemy.screenPos.y, players.enemy.width, players.enemy.height);
-
-//   // Draw player sprite
-//   ctx.drawImage( images[2],players.player.sx ,
-//    players.player.sy, players.player.swidth, players.player.sheight,
-//    players.player.levelPos.x - players.enemy.levelPos.x + players.enemy.screenPos.x, players.player.screenPos.y, players.player.width, players.player.height);
-
-//   ctx.restore();
-// }
 
 // TODO: FIX THIS
 // var camera = {
