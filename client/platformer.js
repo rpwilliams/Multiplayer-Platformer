f2c49840003c35a6b14a04607dd842cca220a897
BioNinja45 <dabob45@gmail.com>
2016-12-14 16:59:50 -0600

    Add files via upload

--- client/platformer.js
+++ client/platformer.js
@@ -5,18 +5,21 @@ var canvas = document.getElementById('screen');
 canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
 //var camera = new Camera(canvas);
-var gameOver = false;
 var images = [
   new Image(),
   new Image(),
   new Image(),
   new Image(),
+  new Image(),
+  new Image()
 ];
 
 images[0].src = 'level.png'; // Background
 images[1].src = 'stars.jpg'; // Foreground
 images[2].src = 'player.png';  // Player
 images[3].src = 'Enemy_Ship.png'; // enemy
+images[4].src = 'enemyBomb.png';
+images[5].src = 'Explosion.png';
 
 var hidingObjImages = [
   new Image(),
@@ -40,7 +43,8 @@ var reticule = {
   x: 0,
   y: 0,
   fire:false,
-  canvas: canvas
+  canvas: canvas,
+  type: ""
 }
 
 /**
@@ -71,7 +75,7 @@ window.onload = function() {
 
   // Handle movement updates from the server
   socket.on('render', function(players, hidingObjects){
-    initObjectsAndPlayers(players, hidingObjects, ctx);
+    init(players, hidingObjects, ctx);
   });
 
   // Handle game on events
@@ -161,21 +165,33 @@ window.onload = function() {
   window.onmousedown = function(event) {
   event.preventDefault();
     if(event.button == 0) {
-      reticule.x = event.offsetX;
-      reticule.y = event.offsetY;
-      reticule.fire=true;
-      socket.emit('fire',reticule);
-      reticule.fire=false;
-    }
+		reticule.x = event.offsetX;
+		reticule.y = event.offsetY;
+		reticule.fire=true;
+		reticule.type="lazer";
+		socket.emit('fire',reticule);
+		reticule.fire=false;
+		
   }
+}
+canvas.oncontextmenu = function(event) {
+  event.preventDefault();
+  reticule.x = event.offsetX;
+  reticule.y = event.offsetY;
+  
+  reticule.fire=true;
+  reticule.type="bomb";
+	socket.emit('fire',reticule);
+	reticule.fire=false;
+}
 }
 
 /**
-  * @function initObjectsAndPlayers()
+  * @function init()
   * Initializes the game, with the player at the beginning of the map and
   * the enemy at the end of the map.
   */
-function initObjectsAndPlayers(players, hidingObjects, ctx)
+function init(players, hidingObjects, ctx)
 {
     // Render the normal objects, followed by the player and then the objects that the player is hiding behind
     renderHidingObjects(players, hidingObjects, ctx, false);
@@ -210,117 +226,121 @@ function renderBackground(ctx, current) {
   * @param ctx: the canvas context to be drawn to
   */
 function renderPlayers(players, ctx) {
-  if(!gameOver)
-  {
-    // Render debug information. TODO: Remove
-    ctx.save();
-    ctx.fillStyle = 'white';
-    ctx.font="20px Verdana";
-    ctx.fillText('level: (' + Math.floor(players.current.levelPos.x) + ',' + Math.floor(players.current.levelPos.y) + ')', players.current.screenPos.x, players.current.screenPos.y - 30);
-    ctx.fillText('screen: (' + Math.floor(players.current.screenPos.x)+ ',' + Math.floor(players.current.screenPos.y) + ')', players.current.screenPos.x, players.current.screenPos.y - 10);
-    ctx.fillText('level: (' + Math.floor(players.other.levelPos.x) + ',' + Math.floor(players.other.levelPos.y) + ')', players.other.levelPos.x - players.current.levelPos.x + players.current.screenPos.x, players.other.screenPos.y - 30);
-    ctx.fillText('screen: (' + Math.floor(players.other.screenPos.x)+ ',' + Math.floor(players.other.screenPos.y) + ')', players.other.levelPos.x - players.current.levelPos.x + players.current.screenPos.x, players.other.screenPos.y - 10);  
-    ctx.restore();
 
-    // Player perspective
-    if(players.current.id=='player') {
-      // Render lasers
-      for (var i = 0 ; i < players.other.enemyFire.length ; i++)
-      {
-        ctx.save();
-        ctx.translate(players.other.enemyFire[i].position.x+players.current.screenPos.x-players.current.levelPos.x, players.other.enemyFire[i].position.y)
-        ctx.fillStyle = "violet";
-        ctx.rotate(-players.other.enemyFire[i].angle);
-        ctx.fillRect(0,0, players.other.enemyFire[i].width, players.other.enemyFire[i].height*3);
-        ctx.restore();
-      }
+  // Render debug information. TODO: Remove
+  ctx.save();
+  ctx.fillStyle = 'white';
+  ctx.font="20px Verdana";
+  ctx.fillText('level: (' + Math.floor(players.current.levelPos.x) + ',' + Math.floor(players.current.levelPos.y) + ')', players.current.screenPos.x, players.current.screenPos.y - 30);
+  ctx.fillText('screen: (' + Math.floor(players.current.screenPos.x)+ ',' + Math.floor(players.current.screenPos.y) + ')', players.current.screenPos.x, players.current.screenPos.y - 10);
+  ctx.fillText('level: (' + Math.floor(players.other.levelPos.x) + ',' + Math.floor(players.other.levelPos.y) + ')', players.other.levelPos.x - players.current.levelPos.x + players.current.screenPos.x, players.other.screenPos.y - 30);
+  ctx.fillText('screen: (' + Math.floor(players.other.screenPos.x)+ ',' + Math.floor(players.other.screenPos.y) + ')', players.other.levelPos.x - players.current.levelPos.x + players.current.screenPos.x, players.other.screenPos.y - 10);  
+  ctx.restore();
 
-      // Draw current player's sprite
-      ctx.drawImage( images[2],players.current.sx,
-        players.current.sy, players.current.swidth, players.current.sheight,
-        players.current.screenPos.x, players.current.screenPos.y, players.current.width, players.current.height);
-
-      // Draw other player's sprite
-      ctx.drawImage( images[3],players.other.sx ,
-        players.other.sy, players.other.swidth, players.other.sheight,
-        players.other.levelPos.x - players.current.levelPos.x + players.current.screenPos.x, players.other.screenPos.y,
-        players.other.width, players.other.height);   
-    }
-
-
-    // Enemy perspective
-    else{
-      // Render lasers
-      for (var i = 0 ; i < players.current.enemyFire.length ; i++){
-        ctx.save();
-        ctx.translate(players.current.enemyFire[i].position.x+players.current.screenPos.x-players.current.levelPos.x, players.current.enemyFire[i].position.y);
-        ctx.fillStyle = "violet";
-        ctx.rotate(-players.current.enemyFire[i].angle);
-        ctx.fillRect(0,0, players.current.enemyFire[i].width, players.current.enemyFire[i].height*3);
-        ctx.restore();
-      }
-
-      // Draw current player's sprite
-      ctx.drawImage( images[3],players.current.sx ,
-        players.current.sy, players.current.swidth, players.current.sheight,
-        players.current.screenPos.x, players.current.screenPos.y, players.current.width, players.current.height);
-
-      // Draw other player's sprite
-      ctx.drawImage( images[2],players.other.sx ,
-        players.other.sy, players.other.swidth, players.other.sheight,
-        players.other.levelPos.x - players.current.levelPos.x + players.current.screenPos.x,
-        players.other.screenPos.y, players.other.width, players.other.height);
-
-      // Draw enemy's reticle
+  // Player perspective
+  if(players.current.id=='player') {
+    // Render lasers
+    for (var i = 0 ; i < players.other.enemyFire.length ; i++)
+    {
       ctx.save();
-      ctx.translate(reticule.x, reticule.y);
-      ctx.beginPath();
-      ctx.arc(0, 0, 10, 0, 2*Math.PI);
-      ctx.moveTo(0, 15);
-      ctx.lineTo(0, -15);
-      ctx.moveTo(15, 0);
-      ctx.lineTo(-15, 0);
-      ctx.strokeStyle = '#00ff00';
-      ctx.stroke();
+      ctx.translate(players.other.enemyFire[i].position.x+players.current.screenPos.x-players.current.levelPos.x, players.other.enemyFire[i].position.y)
+      ctx.fillStyle = "violet";
+      ctx.rotate(-players.other.enemyFire[i].angle);
+      ctx.fillRect(0,0, players.other.enemyFire[i].width, players.other.enemyFire[i].height*3);
       ctx.restore();
     }
+
+    // Draw current player's sprite
+    ctx.drawImage( images[2],players.current.sx,
+      players.current.sy, players.current.swidth, players.current.sheight,
+      players.current.screenPos.x, players.current.screenPos.y, players.current.width, players.current.height);
+
+    // Draw other player's sprite
+    ctx.drawImage( images[3],players.other.sx ,
+      players.other.sy, players.other.swidth, players.other.sheight,
+      players.other.levelPos.x - players.current.levelPos.x + players.current.screenPos.x, players.other.screenPos.y,
+      players.other.width, players.other.height);   
   }
 
-  if(players.current.wonGame || players.other.wonGame)
-  {
-    // Indicate if player 1 won the game by reaching the end
-    if(players.current.id == "player" || players.other.id == "player")
-    {
-      console.log("Player 1 won!");
-      ctx.fillStyle = 'white';
-      ctx.font="40px Verdana";
-      ctx.fontWeight = 'bolder';
-      // Get the center of the screen
-      var midpoint = rectangleMidpoint(0, 0, canvas.width, canvas.height);
-      var X_OFFSET_1 = 100;
-      var X_OFFSET_2 = 100;
-      var Y_OFFSET = 50;
-      // Subtract the midpoint by half of the number of letters of the message
-      // so that the text appears in the center of the midpoint.
-      ctx.fillText('Player 1 wins!', midpoint.x - X_OFFSET_1, midpoint.y);
-      ctx.font="20px Verdana";
-      ctx.fillText('Press space to play again', midpoint.x - X_OFFSET_2, midpoint.y + Y_OFFSET);
-      // gameOver = true;
-      // // Check if space was pressed to restart the game
-      // window.onkeydown = function(event)
-      // {
-      //   if(event.keyCode == 32)
-      //   {
-      //     gameOver = false;
-      //   }
-      // }
 
+  // Enemy perspective
+  else{
+    // Render lasers
+    for (var i = 0 ; i < players.current.enemyFire.length ; i++){
+      ctx.save();
+      ctx.translate(players.current.enemyFire[i].position.x+players.current.screenPos.x-players.current.levelPos.x, players.current.enemyFire[i].position.y);
+      ctx.fillStyle = "violet";
+      ctx.rotate(-players.current.enemyFire[i].angle);
+      ctx.fillRect(0,0, players.current.enemyFire[i].width, players.current.enemyFire[i].height*3);
+      ctx.restore();
     }
-    // Indicate if player 2 won by killing the player
-    else
-    {
-      // player 2 won the game
-    }
+	for (var i = 0 ; i < players.current.enemyBomb.length ; i++)
+  {
+	  console.log(players.current.enemyBomb[i].state);
+	  ctx.save();
+	  
+	  ctx.translate(players.current.enemyBomb[i].position.x+players.current.screenPos.x-players.current.levelPos.x, players.current.enemyBomb[i].position.y);
+		
+	  //ctx.rotate(-players.current.enemyBomb[i].angle);
+	  if(players.current.enemyBomb[i].state=="falling"){
+	  ctx.drawImage(images[4],0,0);
+	  }
+	  
+	  else if(players.current.enemyBomb[i].state!="finished"){
+		ctx.drawImage( images[5],players.current.enemyBomb[i].explosionAnimation*players.current.enemyBomb[i].explosionImageWidth,0 , 
+		players.current.enemyBomb[i].explosionImageWidth,players.current.enemyBomb[i].explosionImageHeight ,0,0,
+		players.current.enemyBomb[i].width ,players.current.enemyBomb[i].height )
+	  }
+	  
+	  ctx.restore();
+  }
+
+    // Draw current player's sprite
+    ctx.drawImage( images[3],players.current.sx ,
+      players.current.sy, players.current.swidth, players.current.sheight,
+      players.current.screenPos.x, players.current.screenPos.y, players.current.width, players.current.height);
+
+    // Draw other player's sprite
+    ctx.drawImage( images[2],players.other.sx ,
+      players.other.sy, players.other.swidth, players.other.sheight,
+      players.other.levelPos.x - players.current.levelPos.x + players.current.screenPos.x,
+      players.other.screenPos.y, players.other.width, players.other.height);
+
+    // Draw enemy's reticle
+    ctx.save();
+    ctx.translate(reticule.x, reticule.y);
+    ctx.beginPath();
+    ctx.arc(0, 0, 10, 0, 2*Math.PI);
+    ctx.moveTo(0, 15);
+    ctx.lineTo(0, -15);
+    ctx.moveTo(15, 0);
+    ctx.lineTo(-15, 0);
+    ctx.strokeStyle = '#00ff00';
+    ctx.stroke();
+    ctx.restore();
+  }
+
+  // Indicate if player 1 won the game by reaching the end
+  if(players.current.wonGame)
+  {
+    console.log("Player 1 won!");
+    ctx.fillStyle = 'white';
+    ctx.font="40px Verdana";
+    ctx.fontWeight = 'bolder';
+    // Get the center of the screen
+    var midpoint = rectangleMidpoint(0, 0, canvas.width, canvas.height);
+    var X_OFFSET_1 = 100;
+    var X_OFFSET_2 = 115;
+    var Y_OFFSET = 50;
+    // Subtract the midpoint by half of the number of letters of the message
+    // so that the text appears in the center of the midpoint.
+    ctx.fillText('Player 1 wins!', midpoint.x - X_OFFSET_1, midpoint.y);
+    ctx.font="20px Verdana";
+    ctx.fillText('Press space to play again', midpoint.x - X_OFFSET_2, midpoint.y + Y_OFFSET);
+  }
+  else if(players.other.wonGame)
+  {
+    // TO DO: Check if enemy killed player and won the game
   }
   ctx.restore();
 }
@@ -384,4 +404,3 @@ function rectangleMidpoint(x1, y1, x2, y2)
   midpoint.y = (y1 + y2)/2;
   return midpoint;
 }
-
